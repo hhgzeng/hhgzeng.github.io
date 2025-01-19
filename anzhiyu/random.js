7f9830fef5efcf4b52ecf7c9372788ff4f9fbe2f
jing zeng <jingzeng@Mac.lan>
2025-01-19 19:22:51 +0800

    Site updated: 2025-01-19 19:22:51

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2025/01/09/EssayLayout/","2025/01/18/GitHub/","2025/01/17/LaTex/","2025/01/17/MySQL/","2025/01/09/Prompt/","2025/01/17/PythonEnv/","2025/01/17/WebDeploy/","2025/01/18/PythonDevelopment/","2025/01/17/READ/","2025/01/19/hello-world/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
