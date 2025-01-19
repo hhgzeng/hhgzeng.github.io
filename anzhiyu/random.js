83dbf180f49380071e41c52945042e5f02285b76
jing zeng <jingzeng@Mac.lan>
2025-01-19 21:20:12 +0800

    Site updated: 2025-01-19 21:20:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/18/GitHub/","2025/01/09/Prompt/","2025/01/09/EssayLayout/","2025/01/17/READ/","2025/01/17/MySQL/","2025/01/19/hello-world/","2025/01/17/LaTex/","2025/01/17/PythonEnv/","2025/01/18/PythonDevelopment/","2025/01/17/WebDeploy/"];function toRandomPost(){
+var posts=["2025/01/18/EssayLayout/","2025/01/18/GitHub/","2025/01/18/Prompt/","2025/01/18/DatabaseNote/","2025/01/18/LaTex/","2025/01/18/MySQL/","2025/01/18/WebDeploy/","2025/01/18/READ/","2025/01/18/PythonEnv/","2025/01/18/PythonDevelopment/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
